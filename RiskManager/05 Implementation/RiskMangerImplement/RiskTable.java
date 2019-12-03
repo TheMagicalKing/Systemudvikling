cbd46471861ee42e7e77d54e4ded21f0d6e23895
simo215d <simo215d@edu.easj.dk>
2019-12-03 09:22:40 +0100

    fixed an error in risk table class

--- RiskManager/05 Implementation/RiskMangerImplement/RiskTable.java
+++ RiskManager/05 Implementation/RiskMangerImplement/RiskTable.java
@@ -3,6 +3,6 @@ import java.util.ArrayList;
 public class RiskTable{
     private ArrayList<Risk> risks = new ArrayList<>();
 
-    public Risk addRisk(){return null}
+    public Risk addRisk(){return null;}
     public void sendRisk(Risk risk, String description, double probability, double consequence){}
 }
