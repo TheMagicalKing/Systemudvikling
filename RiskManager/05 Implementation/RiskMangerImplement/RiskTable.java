d96fcb4ede7b13b25113fd5e88a26629dbd3d52a
simo215d <simo215d@edu.easj.dk>
2019-12-03 09:20:39 +0100

    Risk table first implementation. Made from class diagram.

---
+++ RiskManager/05 Implementation/RiskMangerImplement/RiskTable.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class RiskTable{
+    private ArrayList<Risk> risks = new ArrayList<>();
+
+    public Risk addRisk(){return null}
+    public void sendRisk(Risk risk, String description, double probability, double consequence){}
+}
